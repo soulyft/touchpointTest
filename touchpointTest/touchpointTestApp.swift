cef1256ef8a22ececfe5fc4ced3f735205863bf2
Corey Lofthus <corey@soulyftaudio.com>
2024-07-02 21:09:59 -0700

    Initial Commit

---
+++ touchpointTest/touchpointTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchpointTestApp.swift
+//  touchpointTest
+//
+//  Created by Corey Lofthus on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct touchpointTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
